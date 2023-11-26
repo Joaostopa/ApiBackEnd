250727656e15e98696709073a2cdcb576259c0f2
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2023-11-26 20:37:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,72 @@ const express = require('express');
 
 const server = express();
 
-server.get('/curso/:id', (req, res) =>{
-  const id = req.params.id;
+server.use(express.json());
+
+const futebol =['Time', 'Posicao', 'Numero'];
+
+//Midd Global
+server.use((req, res, next)=>{
+  console.log(`Chamada: ${req.url}`);
+
+  return next();
+});
+
+function checkFut(req,res,next){
+  if(!req.body.name){
+    return res.status(400).json({Erro: "Nome obrigatorio"});
+  }
+
+  return next();
+}
+
+function checkIndexFut(req, res, next){
+
+  if(!futebol[req.params.index]){
+    return res.status(400).json({Erro: "Inexistente"});
+  }
+
+  req.futebol = futebol;
+
+  return next();
+}
+
+server.get('/futebol', (req, res) =>{
+  return res.json(futebol);
+})
+
+server.get('/futebol/:index',  checkIndexFut, (req, res) =>{
+  const { index } = req.params;
   
-  return res.json({ curso: `Aprendendo: ${id}`});
+  return res.json(req.futebol);
 
 })
+
+//Criação 
+server.post('/futebol', checkFut,(req,res) =>{
+  const{ name } = req.body;
+  futebol.push(name);
+
+  return res.json(futebol);
+})
+
+//Alterando 
+server.put('/futebol/:index', checkFut,  checkIndexFut, (req, res) =>{
+  const { index } = req.params;
+  const { name } = req.body;
+
+  futebol[index] = name;
+
+  return res.json(futebol);
+});
+
+//Deletando
+server.delete('/futebol/:index',  checkIndexFut, (req, res)=>{
+  const { index } = req.params;
+
+  futebol.splice(index, 1);
+  return res.json({mensagem: "Deletado com suceso!"});
+})
+
 
 server.listen(3000);

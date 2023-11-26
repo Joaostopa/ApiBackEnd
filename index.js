41e4b47688f66e91b7a482eff4892e87893e0847
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2023-11-26 19:13:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const server = express();
+
+server.get('/curso/:id', (req, res) =>{
+  const id = req.params.id;
+  
+  return res.json({ curso: `Aprendendo: ${id}`});
+
+})
+
+server.listen(3000);

f068fb77e6d16ebfabf8414ed4e1733ef1400173
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2023-12-09 10:47:25 -0300

    Update AcessoControle.js

--- AcessoControle.js
+++ AcessoControle.js
@@ -1,13 +1,20 @@
-import User from '../models/Usuario';
+import Usuario from '../models/Usuario';
 
 class AcessoControle{
-  async store(req,res){
+
+  async store(req, res){
     const { nome } = req.body;
 
-      let user = await User.create({ nome });
+    
+    let usuario = await Usuario.findOne({ nome });
 
-      return res.json(user);
+    if(!usuario){
+      usuario = await Usuario.create({ nome });
+    }
+    
+    return res.json(usuario);
   }
+
 }
 
 export default new AcessoControle();

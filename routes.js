b21eca6ca006dc51b2d6451959ba9a8da26bdd4b
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2023-12-09 10:47:59 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,20 @@
-import { Router }  from  'express';
+import { Router } from 'express';
+
 
 import AcessoControle from './views/AcessoControle';
 import JogadorControle from './views/JogadorControle';
+import Dashboard from './views/Dashboard';
 
 const routes = new Router();
 
-routes.post('/comissao', AcessoControle.store)
+routes.post('/jogadores', JogadorControle.store);
 
-routes.post('/jogador', JogadorControle.store)
+routes.post('/acessos', AcessoControle.store);
+routes.get('/jogadores',JogadorControle.index);
+routes.put('/jogadores/:jogador_id',JogadorControle.update);
+routes.delete('/jogadores',JogadorControle.destroy);
+
+routes.get('/dashboard',Dashboard.show);
+
 
 export default routes;

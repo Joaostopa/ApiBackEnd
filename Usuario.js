898fb479ca10bf83100d618f937e7d5d84148a17
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2023-12-09 10:47:13 -0300

    Update Usuario.js

--- Usuario.js
+++ Usuario.js
@@ -2,8 +2,7 @@ import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
   nome: String,
-  email: String,
-  cpf: Number,
+  senha: String,
 });
 
 export default model('Usuario',UserSchema);

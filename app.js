96b6705328bd86aa206ad163a8cdb4919bd97805
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2023-12-18 13:46:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express ();
 const connectDatabase =('../database/db');
 
-connectDatabase
+connectDatabase ();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
